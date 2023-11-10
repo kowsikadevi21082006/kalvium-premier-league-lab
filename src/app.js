0bf84f19b0b562cda52244c976e477fa850ad5ef
kowsikadevi21082006 <144236845+kowsikadevi21082006@users.noreply.github.com>
2023-11-10 22:01:55 +0530

    done

--- src/app.js
+++ src/app.js
@@ -32,22 +32,79 @@ try {
 }
 
 //Progression 3 - Filter players that debuted in ___ year
+function Filterbd(year){
+  return players.Filter((player)=>player.debut==year)
+}
 
 //Progression 4 - Filter players that play at the position _______
+function Filterbp(position){
+  return player.Filter((player)=> player.position == position)
+}
 
 //Progression 5 - Filter players that have won ______ award
+function Filterba(award){
+  return player.filter((playeraward) =>{
+    return playeraward.award.some((awards)=>awards.name == award)
+  })
+}
 
 //Progression 6 - Filter players that won ______ award ____ times
+function filterbat(award,nooftime){
+  return players.filter((playerat)=>{
+    let awards = playerat.awards.filter((times)=>times.name==awardname)
+    return awards.length === nooftime
+  })
+}
 
 //Progression 7 - Filter players that won ______ award and belong to ______ country
+function filterbac(awardn,country){
+  return players.Filter((playerac)=>{
+    let awardc = playerac.awards.some((awardc)=>awardc.name == awardname)
+    return awardc && playerac.country === country
+  })
+}
 
 //Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+function filterbnoata(noofawards,team,age){
+  return players.filter((playeraa)=>{
+    var ans = playeraa.awards.length >= noofawards && playeraa.team === team && playeraa.age <= age
+    return ans
+  })
+}
 
 //Progression 9 - Sort players in descending order of their age
+function sortByAge(){
+  return players.age.sort(x,y) = y.age - x.age
+}
 
 //Progression 10 - Sort players beloging to _____ team in descending order of awards won
+function FilterByTeamxSortByNoOfAwards(){
+  return players.awards.sort(x,y) = y.awards - x.awards
+}
 
 //Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+function SortByNamexAwardxTimes(awardName, noOfTimes, country){
+  let average = filterByAwardxTimes(awardName,noOfTimes)
+  let output = average.filter(function(item){
+    return item.country === country
+  })
+  return output;
+}
 
 //Challenge 2 - Sort players that are older than _____ years in alphabetical order
 //Sort the awards won by them in reverse chronological order
+function SortByNamexOlderThan(age){
+  let old1 = players.filter(function(item){
+    return item.age > age
+  })
+  old1.sort(function(i){
+    return i.name
+  })
+
+  let old2 =  players.filter(function(item){
+      return item.awards.sort(function(x,y){
+        return x.year - y.year
+      })
+  })
+  return old2
+}
